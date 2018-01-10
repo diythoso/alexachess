c0e61e1eb882766f10a9e065c997b2b76b2774ba
Dzung Vo <vo.tuan.dung@framgia.com>
2018-01-10 11:51:56 +0900

    fix 1
    First commit
    
    fix
    
    fix
    
    fix
    
    add chess framework
    
    fix
    
    fix
    
    fix
    
    fix
    
    fix
    
    test
    
    fix
    
    fix
    
    fix
    
    fix
    
    fix

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 app.use(express.static('public'));
 var http = require('http').Server(app);
+var verifier = require('alexa-verifier')
 var port = process.env.PORT || 3000;
 var io = require('socket.io')(http);
 
@@ -13,12 +14,167 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/public/default.html');
 });
 
+var currentTurn;
+
 io.on('connection', function (socket) {
-    socket.on('new_message', function (data) {
-        // we tell the client to execute 'new message'
-        socket.broadcast.emit('new message', {
-            username: socket.username,
-            message: data
+
+    // initialization
+    io.emit('init');
+
+    // on turn changed
+    socket.on('changeTurn', function(turn) {
+        currentTurn = turn;
+    });
+
+    // verification
+    app.use(function (req, res, next) {
+        if (!req.headers.signaturecertchainurl) {
+            return next();
+        }
+
+        // mark the request body as already having been parsed so it's ignored by 
+        // other body parser middlewares 
+        req._body = true;
+        req.rawBody = '';
+
+        //once we get data, add it on to our body
+        req.on('data', function (data) {
+            return req.rawBody += data;
         });
+
+        //once its all over
+        req.on('end', function () {
+
+            var cert_url, er, error, requestBody, signature;
+
+            //parse the raw body
+            try {
+                req.body = JSON.parse(req.rawBody);
+            } catch (error) {
+                er = error;
+                req.body = {};
+            }
+
+            //get the information needed to verify the request
+            cert_url = req.headers.signaturecertchainurl;
+            signature = req.headers.signature;
+            requestBody = req.rawBody;
+
+            //verify the request
+            verifier(cert_url, signature, requestBody, function (er) {
+
+                if (er) {
+                    //if it fails, throw an error and return a failure
+                    console.error('error validating the alexa cert:', er);
+                    res.status(401).json({ status: 'failure', reason: er });
+                } else {
+                    //proceed
+                    console.log("verified!")
+                    next();
+                }
+            });
+        });
+
+    });
+
+    // Handles the route for echo apis
+    app.post('/api/echo', function (req, res) {
+        var info = req.body.request;
+
+        // Called when all data has been accumulated
+
+        console.log(info.type);
+
+        // parsing the requestBody for information
+        if (info.type == "LaunchRequest") {
+            // crafting a response
+            responseBody = {
+                "version": "0.1",
+                "response": {
+                    "outputSpeech": {
+                        "type": "PlainText",
+                        "text": "Welcome to Voice Chess! Let's get started"
+                    },
+                    "card": {
+                        "type": "Simple",
+                        "title": "Opened",
+                        "content": "Launch application"
+                    },
+                    "reprompt": {
+                        "outputSpeech": {
+                            "type": "PlainText",
+                            "text": "Say a command"
+                        }
+                    },
+                    "shouldEndSession": false
+                }
+            };
+        }
+        else if (info.type == "IntentRequest") {
+            var outputSpeechText = "";
+            var cardContent = "";
+          
+            if (info.intent.name == "MoveIntent") {
+                var target = info.intent.slots.target.value;
+                var fromLetter = info.intent.slots.from_letter.value.toLowerCase();
+                var fromNumber = info.intent.slots.from_number.value;
+                var toLetter = info.intent.slots.to_letter.value.toLowerCase();
+                var toNumber = info.intent.slots.to_number.value;
+                
+                io.emit('move', {
+                    from : fromLetter.charAt(0) + fromNumber,
+                    to: toLetter.charAt(0) + toNumber
+                });
+
+                outputSpeechText = target + "moved to " + toLetter + " " + toNumber;
+                cardContent = "Successfully called " + info.intent.name + ", but it's not implemented!";
+            } else {
+                outputSpeechText = info.intent.name + " not implemented";
+                cardContent = "Successfully called " + info.intent.name + ", but it's not implemented!";
+            }
+            responseBody = {
+                "version": "0.1",
+                "response": {
+                    "outputSpeech": {
+                        "type": "PlainText",
+                        "text": outputSpeechText
+                    },
+                    "card": {
+                        "type": "Simple",
+                        "title": "Move",
+                        "content": cardContent
+                    },
+                    "shouldEndSession": false
+                }
+            };
+        } else {
+            // Not a recognized type
+            responseBody = {
+                "version": "0.1",
+                "response": {
+                    "outputSpeech": {
+                        "type": "PlainText",
+                        "text": "Could not parse data"
+                    },
+                    "card": {
+                        "type": "Simple",
+                        "title": "Error Parsing",
+                        "content": JSON.stringify(requestBody)
+                    },
+                    "reprompt": {
+                        "outputSpeech": {
+                            "type": "PlainText",
+                            "text": "Say a command"
+                        }
+                    },
+                    "shouldEndSession": false
+                }
+            };
+        }
+
+        res.statusCode = 200;
+        res.contentType('application/json');
+        res.send(responseBody);
     });
 });
+
--- public/default.js
+++ public/default.js
@@ -1,105 +1,48 @@
-(function () {
-    // Initialize variables
-    var $window = $(window);
-    var $usernameInput = $('.usernameInput'); // Input for username
-    var $messages = $('.messages'); // Messages area
-    var $inputMessage = $('.inputMessage'); // Input message input box
+$(function () {
 
-    var $loginPage = $('.login.page'); // The login page
-    var $chatPage = $('.chat.page'); // The chatroom page   
-
+    var board;
+    var game;
     var socket = io();
 
-    // Adds the visual chat message to the message list
-    function addChatMessage(data, options) {
-        // Don't fade the message in if there is an 'X was typing'
-        var $typingMessages = getTypingMessages(data);
-        options = options || {};
-        if ($typingMessages.length !== 0) {
-            options.fade = false;
-            $typingMessages.remove();
-        }
+    var handleMove = function (source, target) {
+        var move = game.move({ from: source, to: target });
 
-        var $usernameDiv = $('<span class="username"/>')
-            .text(data.username)
-            .css('color', getUsernameColor(data.username));
-        var $messageBodyDiv = $('<span class="messageBody">')
-            .text(data.message);
+        if (move === null) return 'snapback';
+    };
 
-        var typingClass = data.typing ? 'typing' : '';
-        var $messageDiv = $('<li class="message"/>')
-            .data('username', data.username)
-            .addClass(typingClass)
-            .append($usernameDiv, $messageBodyDiv);
-
-        addMessageElement($messageDiv, options);
-    }
-
-    // Adds a message element to the messages and scrolls to the bottom
-    // el - The element to add as a message
-    // options.fade - If the element should fade-in (default = true)
-    // options.prepend - If the element should prepend
-    //   all other messages (default = false)
-    function addMessageElement(el, options) {
-        var $el = $(el);
-
-        // Setup default options
-        if (!options) {
-            options = {};
-        }
-        if (typeof options.fade === 'undefined') {
-            options.fade = true;
-        }
-        if (typeof options.prepend === 'undefined') {
-            options.prepend = false;
-        }
-
-        // Apply options
-        if (options.fade) {
-            $el.hide().fadeIn(FADE_TIME);
-        }
-        if (options.prepend) {
-            $messages.prepend($el);
-        } else {
-            $messages.append($el);
-        }
-        $messages[0].scrollTop = $messages[0].scrollHeight;
-    }
-    // Sends a chat message
-    function sendMessage() {
-        var message = $inputMessage.val();
-        // Prevent markup from being injected into the message
-        message = cleanInput(message);
-        // if there is a non-empty message and a socket connection
-        if (message && connected) {
-            $inputMessage.val('');
-            addChatMessage({
-                username: username,
-                message: message
-            });
-            // tell server to execute 'new message' and send along one parameter
-            socket.emit('new_message', message);
-        }
-    }
-
-    $window.keydown(function (event) {
-        // Auto-focus the current input when a key is typed
-        if (!(event.ctrlKey || event.metaKey || event.altKey)) {
-            $currentInput.focus();
-        }
-        // When the client hits ENTER on their keyboard
-        if (event.which === 13) {
-            if (username) {
-                sendMessage();
-                typing = false;
-            } else {
-                setUsername();
-            }
-        }
+    socket.on('init', function () {
+        var cfg = {
+            draggable: true,
+            position: 'start',
+            onDrop: handleMove,
+        };
+        board = new ChessBoard('gameBoard', cfg);
+        game = new Chess();
+        socket.emit(game.turn());
     });
 
-    // Whenever the server emits 'new message', update the chat body
-    socket.on('new message', function (data) {
-        addChatMessage(data);
+    socket.on('move', function (data) {
+        var move = game.move({ from: data.from, to: data.to });
+        var turn = game.turn();
+
+        if (move === null) {
+            return 'snapback';
+        }
+        else {
+            board.move(data.from + '-' + data.to);
+            socket.emit(turn);
+        }
+
+        // display turn
+        document.getElementById("debugText").innerHTML = game.turn() === 'b'
+            ? 'Black\'s turn'
+            : 'White\'s turn';
+
+        if (game.in_checkmate())
+        {
+            document.getElementById("debugText").innerHTML = game.turn() === 'b'
+                ? 'White won'
+                : 'Black won';
+        }
     });
 });
